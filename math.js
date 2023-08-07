d37e1d83616dc8f474870909151c9f1746c4c045
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-07 10:56:10 +0530

    feat: added add functionality

--- math.js
+++ math.js
@@ -1,4 +1,10 @@
 // This module is going to contain the add, subtract, multiply and divide functions
+/*
+* * Adds given two numbers and returns the result (JSDoc documentation)
+* @param{number} a
+* @param{number} b
+* @returns the result
+*/
 function add(a,b){
     return a+b;
 }
