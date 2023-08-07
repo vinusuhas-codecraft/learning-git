24ffe183ab3bd565b339b18ab997151f5c56c372
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-07 15:26:27 +0530

    added multiply function

--- math.js
+++ math.js
@@ -1,1 +1,11 @@
 // This module is going to contain the add, subtract, multiply and divide functions
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+function multiply(a,b){
+    return a*b
+}
+
