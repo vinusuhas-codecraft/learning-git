89ac135ab732d9ed3b86aa868a4acda996c6cc59
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-07 11:38:31 +0530

    feat: divide with tests

--- math.js
+++ math.js
@@ -8,3 +8,9 @@
 function divide(a,b){
     return b/a
 }
+//Test code which confirms divide works
+function testAdd(){
+    //Dividing 10 by 2 must be 5
+    const expected=5
+    assert(expected === divide(2,10),"dividing 10 by s must be 5")
+}
