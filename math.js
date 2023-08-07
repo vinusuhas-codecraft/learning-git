22edc18f2733ff47dd9997c4f0b74f88e0dcc8bf
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-07 15:28:06 +0530

    added subtract functionality

--- math.js
+++ math.js
@@ -1,1 +1,10 @@
 // This module is going to contain the add, subtract, multiply and divide functions
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+function subtract(a,b){
+    return b-a
+}
