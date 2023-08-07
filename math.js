d7392c1b2361719b16d6dbe70508ed19484c7588
vinusuhas-codecraft <vinu.dlima@codecraft.co.in>
2023-08-07 11:37:43 +0530

    feat: divide with tests

--- math.js
+++ math.js
@@ -1,1 +1,10 @@
 // This module is going to contain the add, subtract, multiply and divide functions
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+function divide(a,b){
+    return b/a
+}
